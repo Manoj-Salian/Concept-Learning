dfb51eee13716cf5600ccf6ab41485f4f777850b
Manoj-Salian <54059721+Manoj-Salian@users.noreply.github.com>
2021-12-26 14:41:04 +0530

    Add files via upload

---
+++ ContainerWithMostWater.java
@@ -0,0 +1,25 @@
+//https://leetcode.com/problems/container-with-most-water/
+//https://www.youtube.com/watch?v=ZHQg07n_tbg
+public class ContainerWithMostWater {
+
+	public static void main(String[] args) {
+
+		int height[] = {1,8,6,2,5,4,8,3,7};
+		int l=0;
+		int r = height.length-1;
+		int max = 0;
+		while(l<r) {
+			int lh = height[l];
+			int rh=height[r];
+			int min_h = Math.min(lh, rh);
+			
+			max = Math.max(max, min_h * (r-l));
+			if(lh < rh) 
+				l++;
+			else 
+				r--;
+		}
+		System.out.println(max);
+	}
+
+}
