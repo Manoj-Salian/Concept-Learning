490924cb35031ced6c7f3f2ae5b98544b60a90ca
Manoj-Salian <54059721+Manoj-Salian@users.noreply.github.com>
2022-02-12 15:49:43 +0530

    Update LongestStringInDict.java

--- LongestStringInDict.java
+++ LongestStringInDict.java
@@ -1,35 +1,35 @@
 package com.learning;
-
-import java.util.PriorityQueue;
-
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 //https://www.geeksforgeeks.org/find-largest-word-dictionary-deleting-characters-given-string/
 public class LongestStringInDict {
 
 	public static void main(String[] args) {
 		String[] arr = {"ale", "apple", "monkey", "plea"};
 		String str = "abpcplea";
-		PriorityQueue<String> pq = new PriorityQueue<>((a,b) -> b.length() - a.length());
+
+		Arrays.sort(arr,(String s1,String s2) -> s2.length() - s1.length());
 		
-		for(int i=0;i<arr.length;i++) {
-			pq.add(arr[i]);
+		Map<Character,Integer> map = new HashMap<>();
+		
+		for(int i=0;i<str.length();i++) {
+			map.put(str.charAt(i), map.getOrDefault(str.charAt(i), 0)+1);
 		}
 		
-		while(!pq.isEmpty()) {
-			int i;
-			String str1 = pq.poll();
-			for(i=0;i<str1.length();i++) {
-				if(str.indexOf(str1.charAt(i)) !=0) {
+		for(int j=0;j<arr.length;j++) {
+			Map<Character,Integer> tempMap = map;
+			String arrString = arr[j];
+			int k;
+			for(k=0;k<arrString.length();k++) {
+				if(str.indexOf(arrString.charAt(k)) == -1) {
 					break;
 				}
 			}
 			
-			if(str1.length() == i) {
-				System.out.println(str1);
+			if(k == arrString.length()) {
+				System.out.println(arrString);
 			}
 		}
-		
-		System.out.println("String does not match element from dictionery");
-
 	}
-
 }
