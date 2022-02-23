ecd9bff4b9962077eb258156d844c878b648042f
Manoj-Salian <54059721+Manoj-Salian@users.noreply.github.com>
2022-02-23 07:43:28 +0530

    Update IncreasingSubsequenceof3Elements.java

--- IncreasingSubsequenceof3Elements.java
+++ IncreasingSubsequenceof3Elements.java
@@ -5,21 +5,20 @@ package Learning;
 public class IncreasingSubsequenceof3Elements {
 
 	public static void main(String[] args) {
-		int[] a = {15,9,6,4,5,7};
+		//int[] a = {15,9,6,4,5,7};
 		//int[] a = {3,1,10,3,4};
-		//int[] a = {2,1,4,6,8,4};
-		int smallNo = a[0];
-		int count = 1;
-		for(int i=1;i<a.length;i++) {
+		int[] a = {2,1,4,6,8,4};
+		int smallNo = Integer.MAX_VALUE;
+		int secondSmallNo  = Integer.MAX_VALUE;
+		for(int i=0;i<a.length;i++) {
 			if(a[i] < smallNo) {
 				smallNo= a[i];
+			}else if(a[i] < secondSmallNo){
+				secondSmallNo = a[i];
 			}else {
-				count++;
+				System.out.println("Sequence found : " + smallNo +" " +secondSmallNo+ " "+a[i]);
 			}
-			if(count == 3) {
-				System.out.println("found");
-				break;
-			}
+			
 		}
 					
 	}
